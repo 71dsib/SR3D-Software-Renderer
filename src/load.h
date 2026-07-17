2905207e0a04b196c10917fde374f1c76ca31379
Sudin Bhetwal <125322588+71dsib@users.noreply.github.com>
2026-07-17 11:23:00 -0500

    Added OBJ Hierarchy Support

--- src/load.h
+++ src/load.h
@@ -9,6 +9,7 @@ namespace load{
 namespace load{
     int loadTexture(std::string filePath);
     int loadOBJModel(std::string filePath, int coordinateSys, bool extractNormals);
+    std::vector<int> loadOBJModelHeirarchy(std::string filePath, int coordinateSys, bool extractNormals);
 }
 
 #endif
