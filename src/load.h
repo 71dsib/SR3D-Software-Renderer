ca26992dd686fa199b0edb67cbafe1d8adcacb00
Sudin Bhetwal <125322588+71dsib@users.noreply.github.com>
2026-08-07 07:51:54 -0500

    Update load.h

--- src/load.h
+++ src/load.h
@@ -2,13 +2,9 @@
 #define LOAD_H_INCLUDED
 
 namespace load{
-    inline std::vector<render::model::Texture> loadedTextures {};
-    inline std::vector<render::model::Model> loadedModels {};
-}
-namespace load{
-    int loadTexture(std::string filePath);
-    int loadOBJModel(std::string filePath, int coordinateSys, bool extractNormals);
-    std::vector<int> loadOBJModelHeirarchy(std::string filePath, int coordinateSys, bool extractNormals);
+    int loadTexture(std::string filePath, std::vector<render::model::Texture>& loadedTextures);
+    int loadOBJModel(std::string filePath, int coordinateSys, bool extractNormals, std::vector<render::model::Model>& loadedModels);
+    std::vector<int> loadOBJModelHeirarchy(std::string filePath, int coordinateSys, bool extractNormals, std::vector<render::model::Model>& loadedModels);
 }
 
 #endif
